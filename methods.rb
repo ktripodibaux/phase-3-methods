2dd450916d42280c2efe1970da3c3a82f06c136f
Kurt Baux <ktripodibaux@gmail.com>
2022-07-18 18:09:56 -0400

    commit tree 961313bf14aaf5602357acf478cb3f7eb96e684f

--- methods.rb
+++ methods.rb
@@ -1,1 +1,22 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default name
+    if name
+        puts "Hello, #{name}!"
+    end
+
+    puts "Hello, programmer!"
+end
+
+def add (num1,num2)
+    num1+num2
+end
+
